59dcae33388ed13ada67e0333c33e6ba5ec34ddb
benyt9 <benlucasft@gmail.com>
2025-09-18 17:08:43 +0200

    Initial commit

---
+++ src/main/java/bne/bneplugins/bnehomes/BneHomes.java
@@ -0,0 +1,17 @@
+package bne.bneplugins.bnehomes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BneHomes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
